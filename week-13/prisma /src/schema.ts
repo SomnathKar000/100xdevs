9a7f7a8768444ce1d109b4d9f46a92f7667bdefa
Somnath Kar <somnathkar201901@gmail.com>
2025-05-01 19:31:44 +0530

    prisma model review

---
+++ week-13/prisma /src/schema.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.$queryRaw`CREATE DATABASE prisma_test_2`;
+}
+
+main()
+  .then(async () => {
+    console.log("Executed successfully");
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.log("Got an error ", e);
+    await prisma.$disconnect();
+  });
