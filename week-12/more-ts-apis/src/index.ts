671bec8dc81973f920d44b984ae1e27b5cc1e6b6
Somnath Kar <somnathkar201901@gmail.com>
2025-07-28 22:02:36 +0530

    Usecase of Pick

--- week-12/more-ts-apis/src/index.ts
+++ week-12/more-ts-apis/src/index.ts
@@ -1,21 +1,14 @@
 interface User {
+  id: string;
   name: string;
-  age: number;
+  age?: number;
+  email: string;
+  password: string;
+  createdAt?: Date;
 }
 
-function calculateAge(user1: User, user2: User): number {
-  return user1.age + user2.age;
+type UserProfile = Pick<User, "name" | "age" | "password">;
+
+function updateUser(user: UserProfile) {
+  // Updates user
 }
-
-const test1 = {
-  name: "test 1",
-  age: 12,
-};
-const test2 = {
-  name: "Test 2",
-  age: 18,
-};
-
-const totalAge = calculateAge(test1, test2);
-
-console.log(totalAge);
