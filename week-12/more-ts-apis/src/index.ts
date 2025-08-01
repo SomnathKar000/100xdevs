99ef28bc70432d5c6400a11355e18a00e68e1ea2
Somnath Kar <somnathkar201901@gmail.com>
2025-08-01 22:19:34 +0530

    basic zod example

--- week-12/more-ts-apis/src/index.ts
+++ week-12/more-ts-apis/src/index.ts
@@ -1,12 +1,45 @@
-interface User {
-  name: string;
-  email: string;
-  password: string;
-}
+import express from "express";
+import { z } from "zod";
 
-type UserExclude = Pick<User, "email" | "password">;
+const app = express();
 
-const user: UserExclude = {
-  email: "xyz@gmail.com",
-  password: "23123",
-};
+app.use(express.json());
+
+const UserProfileSchema = z.object({
+  name: z.string().min(3, { message: "Name cannot be empty" }),
+  email: z.string().email({ message: "Invalid email format" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must contain minimum 8 characters" }),
+  age: z
+    .number()
+    .min(18, { message: "Age must be greater than 18" })
+    .optional(),
+});
+
+app.get("health", (req, res) => {
+  res.json({
+    status: true,
+    message: "Working fine!",
+  });
+});
+
+app.put("/user", (req, res) => {
+  const { success, error } = UserProfileSchema.safeParse(req.body);
+
+  if (!success) {
+    res.status(411).json({
+      status: false,
+      message: error?.message,
+    });
+    return;
+  }
+  const userData = req.body;
+  res.json({
+    status: true,
+    message: "User created successFully",
+    user: userData,
+  });
+});
+
+app.listen(8000);
